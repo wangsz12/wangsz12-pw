42613fd49f3cad023083e4df026b1e34eaa143b0
wangsz12 <wangsz12@foxmail.com>
2024-03-15 02:09:16 +0800

    ✨ feat(主页): 主页UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
 import './home.css';
+import NavBar from './components/home/NavBar';
+import { Metadata } from 'next';
+import Intro from './components/home/Intro';
+import Divider from './components/home/Divider';
+import Image from 'next/image';
+
+export const metadata: Metadata = {
+  title: '首页 - wangsz12',
+};
 
 export default function Home() {
-  return <div className='home-bg h-screen w-screen'>
-    
-  </div>;
+  return (
+    <>
+      <NavBar />
+      <Intro />
+      <Divider>Gallery</Divider>
+      <Divider>Blogs</Divider>
+      <Divider />
+    </>
+  );
 }
