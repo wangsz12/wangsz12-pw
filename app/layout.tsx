8f8d3d1ec4df254a4482b51a72410a9bbd0ad848
wangsz12 <wangsz12@foxmail.com>
2024-03-15 19:43:21 +0800

    ✨ feat(主页): 主页样式修改

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,19 +2,19 @@ import './globals.css';
 import { noto } from './plugins/fonts';
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="zh-Hans">
-      <body className={noto.className}>
-        <div className='home-bg h-screen w-screen'>
-          <div className='px-44'>
-            {children}
-          </div>
-        </div>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="zh-Hans">
+            <body className={noto.className}>
+                <div className='home-bg'>
+                    <div className='px-44'>
+                        {children}
+                    </div>
+                </div>
+            </body>
+        </html>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,34 @@ import { Metadata } from 'next';
 import Intro from './components/home/Intro';
 import Divider from './components/home/Divider';
 import Image from 'next/image';
+import Waterfall from './components/home/Waterfall';
 
 export const metadata: Metadata = {
-  title: '首页 - wangsz12',
+    title: '首页 - wangsz12',
 };
 
 export default function Home() {
-  return (
-    <>
-      <NavBar />
-      <Intro />
-      <Divider>Gallery</Divider>
-      <Divider>Blogs</Divider>
-      <Divider />
-    </>
-  );
+    const mockImageData = [
+        'https://aaronsblog.oss-cn-shanghai.aliyuncs.com/image/%E5%8F%8C%E5%A1%94%E6%89%AB%E8%A1%97/1.jpg',
+        'https://aaronsblog.oss-cn-shanghai.aliyuncs.com/image/%E5%8F%8C%E5%A1%94%E6%89%AB%E8%A1%97/2.jpg',
+        'https://aaronsblog.oss-cn-shanghai.aliyuncs.com/image/%E5%8F%8C%E5%A1%94%E6%89%AB%E8%A1%97/3.jpg',
+        'https://aaronsblog.oss-cn-shanghai.aliyuncs.com/image/%E5%8F%8C%E5%A1%94%E6%89%AB%E8%A1%97/4.jpg',
+        'https://aaronsblog.oss-cn-shanghai.aliyuncs.com/image/%E5%8F%8C%E5%A1%94%E6%89%AB%E8%A1%97/5.jpg',
+        'https://aaronsblog.oss-cn-shanghai.aliyuncs.com/image/%E5%8F%8C%E5%A1%94%E6%89%AB%E8%A1%97/6.jpg',
+    // 'https://aaronsblog.oss-cn-shanghai.aliyuncs.com/image/%E5%8F%8C%E5%A1%94%E6%89%AB%E8%A1%97/7.jpg',
+    // 'https://aaronsblog.oss-cn-shanghai.aliyuncs.com/image/%E5%8F%8C%E5%A1%94%E6%89%AB%E8%A1%97/8.jpg',
+    ];
+
+    return (
+        <>
+            <NavBar />
+            <Intro />
+            <Divider>Gallery</Divider>
+            <Waterfall>
+                {mockImageData}
+            </Waterfall>
+            <Divider>Blogs</Divider>
+            {/* <Divider /> */}
+        </>
+    );
 }
