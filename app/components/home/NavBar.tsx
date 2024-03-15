33a4be4fecc002a0d88a38faff3b3cd4cf4099dd
wangsz12 <wangsz12@foxmail.com>
2024-03-15 19:45:00 +0800

    ✨ feat(导航栏): 导航栏调整

--- app/components/home/NavBar.tsx
+++ app/components/home/NavBar.tsx
@@ -8,7 +8,7 @@ export default function NavBar(props: Props) {
         <nav className="w-full h-28 flex justify-center items-center px-10">
             <NavItem to='/about'>关于</NavItem>
             <NavItem to='/gallery'>摄影</NavItem>
-            <Image className='mx-10' src='/signature.svg' height={90 * 90 / 232} width={90} alt='Simon' />
+            <Image className='mx-10 h-auto w-24' src='/signature.svg' height={0} width={0} alt='Simon' />
             <NavItem to='/blogs'>文章</NavItem>
             <NavItem to='/contact'>联系</NavItem>
         </nav>
