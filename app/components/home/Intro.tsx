2e79daf9475bfa1c66540f698b145df81fc7a21f
wangsz12 <wangsz12@foxmail.com>
2024-03-15 19:44:33 +0800

    ✨ feat(主页): 介绍部分增加介绍语

--- app/components/home/Intro.tsx
+++ app/components/home/Intro.tsx
@@ -2,8 +2,9 @@ import { deYiHei } from '@/app/plugins/fonts';
 
 export default function Intro() {
     return (
-        <div className='h-80 flex justify-center items-center'>
-            <span className={`${deYiHei.className} text-6xl font-light text-gray-700`}>Web开发 / 摄影 / 游戏</span>
+        <div className='h-96 flex flex-col justify-center items-center'>
+            <span className={`${deYiHei.className} text-5xl text-gray-700 mb-10`}>Hi, 这里是Simon</span>
+            <span className={'text-3xl text-gray-500'}>Web开发 / 摄影 / 游戏</span>
         </div>
     );
 }
