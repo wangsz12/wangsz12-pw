9efc9e29da1ad50f9d30ed1c74eae1d13f1e730d
wangsz12 <wangsz12@foxmail.com>
2024-03-18 17:22:07 +0800

    🔧 build: 修改配置

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'aaronsblog.oss-cn-shanghai.aliyuncs.com'
+            },
+        ],
+    },
+};
 
 export default nextConfig;
