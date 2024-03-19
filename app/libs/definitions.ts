afb6148e008c03ef7f1e0468fbea43291e5ca446
wangsz12 <wangsz12@foxmail.com>
2024-03-20 01:42:54 +0800

    ✨ feat: 新增类型定义

--- app/libs/definitions.ts
+++ app/libs/definitions.ts
@@ -1,4 +1,21 @@
+import { Dayjs } from 'dayjs';
+
 export type Tag = {
     id: string;
     name: string;
+}
+
+export type TimelineItem = {
+    from: Dayjs;
+    to?: Dayjs;
+    school: string;
+    location: string;
+    major?: string;
+    degree: string;
+}
+
+export type GalleryImage = {
+    url: string;
+    height: number;
+    width: number;
 }
