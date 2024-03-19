0b9f8219d57c14e3ee1a28474a96c4b1a943aa5e
wangsz12 <wangsz12@foxmail.com>
2024-03-20 01:36:17 +0800

    🦄 refactor(导航栏): 合并导航栏item到导航栏代码文件中

--- app/components/navbar/NavBar.tsx
+++ app/components/navbar/NavBar.tsx
@@ -1,14 +1,31 @@
 import Image from 'next/image';
-import NavItem from './NavItem';
+import Link from 'next/link';
+import './navbar.css';
 
 interface Props {}
+
+interface NavItemProps {
+    children: string;
+    to: string;
+}
+
+const NavItem = (props: NavItemProps) => (
+    <div className='mx-10 flex justify-center'>
+        <Link
+            className='text-gray-800 text-xl tracking-widest item'
+            href={props.to}
+        >{props.children}</Link>
+    </div>
+);
 
 export default function NavBar(props: Props) {
     return (
         <nav className="w-full h-28 flex justify-center items-center px-10">
             <NavItem to='/about'>关于</NavItem>
             <NavItem to='/gallery'>摄影</NavItem>
-            <Image className='mx-10 h-auto w-24' src='/signature.svg' height={0} width={0} alt='Simon' />
+            <Link href='/'>
+                <Image className='mx-10 h-auto w-24' src='/signature.svg' height={0} width={0} alt='Simon' />
+            </Link>
             <NavItem to='/blogs'>文章</NavItem>
             <NavItem to='/contact'>联系</NavItem>
         </nav>
